eeadedc940f69ef6adee9600fd90a3de5694c209
Process-Felix-Hu <stjimmy149@live.com.au>
2022-11-13 14:40:23 +1100

    added stylesheet

--- components/globals/footerComponent.tsx
+++ components/globals/footerComponent.tsx
@@ -3,16 +3,9 @@ import Image from 'next/image'
 export default function FooterComponent(){
   return(
     <footer className="c-footer">
-         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className='c-footer__logo'>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+      <div className='c-footer__inner'>
+        
+      </div>
     </footer>
   )
 }
